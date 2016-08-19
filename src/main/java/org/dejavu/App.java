b77503d69afa742b2dedf5b4a6c20355ac6dfd9f
Wei He <MrQunma@outlook.com>
2016-08-19 17:18:03 +0800

    test file

--- src/main/java/org/dejavu/App.java
+++ src/main/java/org/dejavu/App.java
@@ -1,5 +1,6 @@
 package org.dejavu;
 
+import org.dejavu.validate.*;
 /**
  * Hello world!
  *
@@ -8,6 +9,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       System.out.println(Validate.notNull(null, "The object must not be null"));
     }
 }
