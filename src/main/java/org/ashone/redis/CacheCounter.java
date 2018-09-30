6372da651a2f7ea8bc781659453ef55194b917b7
hewei <mrqunma@outlook.com>
2018-10-01 00:31:39 +0800

    根据基础的不变量设置Redis计数器分片大小，并增加自定义分片大小

--- src/main/java/org/ashone/redis/CacheCounter.java
+++ src/main/java/org/ashone/redis/CacheCounter.java
@@ -1,4 +1,31 @@
 package org.ashone.redis;
 
+import org.ashone.BaseConstance;
+
+import java.util.ArrayList;
+
 public class CacheCounter {
+
+  private ArrayList<Long> precisions = new ArrayList<Long>();
+
+  CacheCounter() {
+    precisions.add(BaseConstance.DAY_MILLIS);
+    precisions.add(BaseConstance.HOUR_MILLIS);
+    precisions.add(BaseConstance.MINUTE_MILLIS);
+    precisions.add(BaseConstance.MONTH_MILLIS);
+  }
+
+  /**
+   * 为计数器新增自定义时间分片单位
+   * @param precision 时间分片大小，单位为毫秒
+   */
+  public void setPrecision(Long precision) {
+    precisions.add(precision);
+  }
+
+  // 计数增加
+  public static void plusCount(String name, int count) {
+    long Millis = System.currentTimeMillis();
+
+  }
 }
