5822a268c52a4e4909a516accfc1e83553d533e7
hewei <mrqunma@outlook.com>
2018-10-01 01:13:06 +0800

    计数器自增功能完成

--- src/main/java/org/ashone/redis/CacheCounter.java
+++ src/main/java/org/ashone/redis/CacheCounter.java
@@ -1,14 +1,20 @@
 package org.ashone.redis;
 
+import com.google.common.math.LongMath;
+import com.lambdaworks.redis.api.sync.RedisCommands;
 import org.ashone.BaseConstance;
 
+import java.math.RoundingMode;
 import java.util.ArrayList;
 
 public class CacheCounter {
 
   private ArrayList<Long> precisions = new ArrayList<Long>();
+  private RedisCommands<String, String> commands;
 
-  CacheCounter() {
+  CacheCounter(String host, int port) {
+    commands = Connection.getSyncCommands(Connection.getConnection(host, port));
+
     precisions.add(BaseConstance.DAY_MILLIS);
     precisions.add(BaseConstance.HOUR_MILLIS);
     precisions.add(BaseConstance.MINUTE_MILLIS);
@@ -17,15 +23,26 @@ public class CacheCounter {
 
   /**
    * 为计数器新增自定义时间分片单位
+   *
    * @param precision 时间分片大小，单位为毫秒
    */
   public void setPrecision(Long precision) {
     precisions.add(precision);
   }
 
-  // 计数增加
-  public static void plusCount(String name, int count) {
-    long Millis = System.currentTimeMillis();
-
+  /**
+   * 计数器自增
+   * @param name 计数分组名称
+   * @param id 计数分组ID
+   * @param count 增加数
+   */
+  public void plusCount(String name, String id, int count) {
+    Long millis = System.currentTimeMillis();
+    for (Long precision : precisions) {
+      // 当前所在的时间片
+      long precisionNow = LongMath.divide(millis, precision, RoundingMode.DOWN);
+      String key = precision + ":" + id;
+      commands.hincrby(name + ":count:" + key, precisionNow + "", count);
+    }
   }
 }
