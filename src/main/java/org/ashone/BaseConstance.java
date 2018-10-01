b3e812132e71c21e7e289f748947e86e8670a530
hewei <mrqunma@outlook.com>
2018-10-01 16:22:23 +0800

    修改时间单位 新增获取计数器方法

--- src/main/java/org/ashone/BaseConstance.java
+++ src/main/java/org/ashone/BaseConstance.java
@@ -1,8 +1,8 @@
 package org.ashone;
 
 public class BaseConstance {
-  public static final long MINUTE_MILLIS = 60000L;
-  public static final long HOUR_MILLIS = 3600000L;
-  public static final long DAY_MILLIS = 86400000L;
-  public static final long MONTH_MILLIS = 2592000000L;
+  public static final long MINUTE_MILLIS = 60L;
+  public static final long HOUR_MILLIS = 3600L;
+  public static final long DAY_MILLIS = 86400L;
+  public static final long MONTH_MILLIS = 2592000L;
 }
--- src/main/java/org/ashone/redis/CacheCounter.java
+++ src/main/java/org/ashone/redis/CacheCounter.java
@@ -7,6 +7,8 @@ import org.ashone.BaseConstance;
 
 import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CacheCounter {
 
@@ -31,16 +33,20 @@ public class CacheCounter {
   }
 
   /**
+   * <p>
    * 为计数器新增自定义时间分片单位
+   * </p>
    *
-   * @param precision 时间分片大小，单位为毫秒
+   * @param precision 时间分片大小，单位为秒
    */
   public void setPrecision(Long precision) {
     precisions.add(precision);
   }
 
   /**
+   * <p>
    * 计数器自增
+   * </P>
    *
    * @param group 计数分组名称
    * @param tag   计数分组ID
@@ -53,6 +59,44 @@ public class CacheCounter {
       long precisionNow = LongMath.divide(millis, precision, RoundingMode.DOWN) * precision;
       String key = precision + ":" + tag;
       commands.hincrby(group + ":count:" + key, precisionNow + "", count);
+      // 将计数器的引用信息放入有序集合中，将其分值设置为0
+      commands.zadd("know:", 0, key);
     }
+  }
+
+  /**
+   * <p>
+   * 得到计数器的值
+   * </P>
+   *
+   * @param group     计数器分组名称
+   * @param tag       计数器分组ID
+   * @param precision 时间分片
+   * @param timestamp 时间戳
+   * @return 当前计数器的值
+   */
+  public long getCounter(String group, String tag, String precision, String timestamp) {
+    String key = group + ":count:" + precision + ":" + tag;
+    String count = commands.hget(key, timestamp);
+    return count != null ? Long.parseLong(count) : 0;
+  }
+
+  /**
+   * <p>
+   * 获取所有计数器的值
+   * </P>
+   *
+   * @param group     计数器分组名称
+   * @param tag       计数器分组ID
+   * @param precision 时间分片
+   * @return 所有计数器的值
+   */
+  public Map<String, String> getAllCounter(String group, String tag, String precision) {
+    String key = group + ":count:" + precision + ":" + tag;
+    return commands.hgetall(key);
+  }
+
+  public void cleanCounter() {
+    
   }
 }
