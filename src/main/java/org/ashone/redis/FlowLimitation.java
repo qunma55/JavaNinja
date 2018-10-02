b248fce8b4ceba1d72947ec52117c57fde6ea4af
hevigo9 <sweeperhevi@gmail.com>
2018-10-02 23:26:42 +0800

    实现一般用途的限流器

--- src/main/java/org/ashone/redis/FlowLimitation.java
+++ src/main/java/org/ashone/redis/FlowLimitation.java
@@ -1,5 +1,6 @@
 package org.ashone.redis;
 
+import com.lambdaworks.redis.Range;
 import com.lambdaworks.redis.api.sync.RedisCommands;
 
 /**
@@ -21,16 +22,25 @@ public class FlowLimitation {
   /**
    * <p>
    * 判断用户能否执行某项操作
+   * 适用于较小流量的判断工作，否则会大量消耗内存空间
    * </P>
    *
    * @param principal       用户主体
-   * @param actionKey       动作
-   * @param period          时间分片
+   * @param actionKey       动作，每个操作对应唯一的动作Str
+   * @param period          时间分片，单位为秒
    * @param allowedMaxCount 允许最大操作次数
    * @return true表示允许执行动作，反之则不允许
    */
   public boolean isActionAllowed(String principal, String actionKey, int period, int allowedMaxCount) {
-
-    return true;
+    String key = String.format("hist:%s:%s", principal, actionKey);
+    long now = System.currentTimeMillis();
+    // 添加一条动作
+    commands.zadd(key, now, now + "");
+    // 清除时间窗口外的动作记录
+    commands.zremrangebyscore(key, Range.create(0, now - period * 1000));
+    // 获取窗口内的记录数量
+    Long count = commands.zcard(key);
+    commands.expire(key, period + 1);
+    return count <= allowedMaxCount;
   }
 }
